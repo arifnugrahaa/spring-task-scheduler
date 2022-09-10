e08a09c341e69ef9cb7ef705a0fe76de8ca28067
arifnugrahaa <arifnugraha1243@gmail.com>
2022-09-10 21:43:30 +0700

    [first commit]

---
+++ src/main/java/com/scheduler/demoscheduler/DemoSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.scheduler.demoscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSchedulerApplication.class, args);
+	}
+
+}
